2285edd0dd20c198010d7487590785f7550357ae
Vhs4 <vhszera44444@gmail.com>
2023-11-22 14:13:57 -0300

    FEAT: Criação da paginação por slide

--- js/slide.js
+++ js/slide.js
@@ -10,6 +10,7 @@ export class Slide {
             movement: 0
         }
         this.activeClass = 'active';
+        this.changeEvent = new Event('changeEvent');
     }
 
     transition(active) {
@@ -28,13 +29,13 @@ export class Slide {
 
     onStart(event) {
         let movetype;
-        if(event.type === 'mousedown') {
-        event.preventDefault();
-        this.dist.startX = event.clientX;
-        movetype = 'mousemove';
+        if (event.type === 'mousedown') {
+            event.preventDefault();
+            this.dist.startX = event.clientX;
+            movetype = 'mousemove';
         } else {
-        this.dist.startX = event.changedTouches[0].clientX;
-        movetype = 'touchmove'
+            this.dist.startX = event.changedTouches[0].clientX;
+            movetype = 'touchmove'
         }
 
         this.wrapper.addEventListener(movetype, this.onMove);
@@ -43,7 +44,7 @@ export class Slide {
 
     onMove(event) {
         const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0]
-        .clientX;
+            .clientX;
         const finalPosition = this.updatePosition(pointerPosition);
         this.moveSlide(finalPosition);
     }
@@ -57,9 +58,9 @@ export class Slide {
     }
 
     changeSlideOnEnd() {
-        if(this.dist.movement > 120 && this.index.next !== undefined) {
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
             this.activeNextSlide();
-        } else if(this.dist.movement < -120 && this.index.prev !== undefined) {
+        } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
             this.activePrevSlide();
         } else {
             this.changeSlide(this.index.active);
@@ -103,6 +104,7 @@ export class Slide {
         this.slidesIndexNav(index);
         this.dist.finalPosition = activeSlide.position;
         this.changeActiveClass();
+        this.wrapper.dispatchEvent(this.changeEvent);
     }
 
     changeActiveClass() {
@@ -111,11 +113,11 @@ export class Slide {
     }
 
     activePrevSlide() {
-        if(this.index.prev !== undefined) this.changeSlide(this.index.prev);
+        if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
     }
 
     activeNextSlide() {
-        if(this.index.next !== undefined) this.changeSlide(this.index.next);
+        if (this.index.next !== undefined) this.changeSlide(this.index.next);
     }
 
     onResize() {
@@ -150,6 +152,11 @@ export class Slide {
 }
 
 export class SlideNav extends Slide {
+    constructor(slide, wrapper) {
+        super(slide, wrapper);
+        this.bindControlEvents();
+    }
+
     addArrow(prev, next) {
         this.prevElement = document.querySelector(prev);
         this.nextElement = document.querySelector(next);
@@ -159,5 +166,41 @@ export class SlideNav extends Slide {
     addArrowEvent() {
         this.prevElement.addEventListener('click', this.activePrevSlide);
         this.nextElement.addEventListener('click', this.activeNextSlide);
+    }
+
+    createControl() {
+        const control = document.createElement('ul');
+        control.dataset.control = 'slide';
+        this.slideArray.forEach((item, index) => {
+            control.innerHTML += `<li><a href="#slide${index + 1}">${index + 1}</a></li>`;
+        });
+        this.wrapper.appendChild(control);
+        return control;
+    }
+
+    eventControl(item, index) {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            this.changeSlide(index);
+        });
+        this.wrapper.addEventListener('changeEvent', this.activeControlItem);
+    }
+
+    activeControlItem() {
+        this.controlArray.forEach(item => item.classList.remove(this.activeClass));
+        this.controlArray[this.index.active].classList.add(this.activeClass);
+    }
+
+    addControl(customControl) {
+        this.control = document.querySelector(customControl) || this.createControl();
+        this.controlArray = [...this.control.children];
+
+        this.activeControlItem();
+        this.controlArray.forEach((item, index) => this.eventControl(item, index));
+    }
+
+    bindControlEvents() {
+        this.eventControl = this.eventControl.bind(this);
+        this.activeControlItem = this.activeControlItem.bind(this);
     }
 }
